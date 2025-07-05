09af1e39905b651777ee999faaf9ecfca7297e8b
veerbal1 <veerbalsingh1@gmail.com>
2025-07-06 00:51:01 +0530

    Created basic header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="w-full h-full">
-      <h1>Web Based Crypto Wallet</h1>
+    <div className="min-h-screen w-full flex  justify-center bg-background">
+      <Header />
     </div>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+
+export default function Header() {
+  return (
+    <div className="max-w-4xl w-full mx-auto p-8">
+      <div className="flex items-center justify-between">
+        <div className="space-y-2">
+          <h1 className="text-3xl font-bold tracking-tight text-foreground">
+            Web based Crypto Wallet
+          </h1>
+          <p className="text-muted-foreground text-lg">
+            for learning purposes
+          </p>
+        </div>
+        
+        <Button size="lg">Create new Wallet</Button>
+      </div>
+    </div>
+  );
+} 
